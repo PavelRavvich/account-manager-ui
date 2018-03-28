93133cc0ebac3e09a203525d558acf677973f600
Pavel <triodjangopiter@yandex.ru>
2018-03-28 03:55:34 +0300

    filter completed

--- src/app/system/vds-list/vds-list.component.ts
+++ src/app/system/vds-list/vds-list.component.ts
@@ -1,5 +1,7 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+import * as moment from 'moment';
+
 import {Vds} from '../shared/model/vds.model';
 import {FormControl, FormGroup} from '@angular/forms';
 import {VdsService} from '../shared/services/vds.service';
@@ -34,25 +36,76 @@ export class VdsListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.filterForm = new FormGroup({
-      'ip': new FormControl(null, []),
-      'id': new FormControl(null, []),
-      'dateFrom': new FormControl(null, []),
-      'dateTo': new FormControl(null, [])
-    });
+    this.updateFilterInstance();
+    this.fillDataSource();
+  }
 
+  applyFilter() {
+    const {ip, id} = this.filterForm.value;
+    this.filterByDate();
+    this.filterById(id);
+    this.filterByIp(ip);
+    console.log(this.dataSource.data);
+  }
+
+  disableFilter() {
+    this.fillDataSource();
+    this.updateFilterInstance();
+  }
+
+  private filterByDate() {
+    const {dateFrom, dateTo, byActivate} = this.filterForm.value;
+    if (!!dateFrom && dateFrom.length !== 0 && !!dateTo && dateTo.length !== 0) {
+      this.dataSource.data = (byActivate === 'activate') ?
+        this.filterActivateBetween(dateFrom, dateTo) :
+        this.filterDeactivateBetween(dateFrom, dateTo);
+    }
+  }
+
+  private filterActivateBetween(dateFilterFrom: string, dateFilterTo: string) {
+    const filterFrom = moment(dateFilterFrom);
+    const filterTo = moment(dateFilterTo);
+    return this.dataSource.data.filter((vds: Vds) => {
+      const activate = moment(vds.activatedDate, 'DD.MM.YYYY');
+      return activate.isBetween(filterFrom, filterTo);
+    });
+  }
+
+  private filterDeactivateBetween(dateFilterFrom: string, dateFilterTo: string) {
+    const filterFrom = moment(dateFilterFrom);
+    const filterTo = moment(dateFilterTo);
+    return this.dataSource.data.filter((vds: Vds) => {
+      const activate = moment(vds.deactivatedDate, 'DD.MM.YYYY');
+      return activate.isBetween(filterFrom, filterTo);
+    });
+  }
+
+  private filterById(id: string): void {
+    if (!!id && id !== '') {
+      this.dataSource.data = [this.dataSource.data.find((vds: Vds) => (vds.id + '') === id)];
+    }
+  }
+
+  private filterByIp(ip: string): void {
+    this.dataSource.data = this.dataSource.data
+      .filter((vds: Vds) => vds.ip.indexOf(ip) !== -1);
+  }
+
+  private fillDataSource() {
     this.vdsService.getVds().subscribe((vds: Vds[]) => {
       this.dataSource = new MatTableDataSource(vds);
       this.dataIsLoaded = true;
     });
   }
 
-  applyFilter() {
-    console.log(this.filterForm);
-  }
-
-  disableFilter() {
-    console.log(this.filterForm);
+  private updateFilterInstance(): void {
+    this.filterForm = new FormGroup({
+      'ip': new FormControl(null, []),
+      'id': new FormControl(null, []),
+      'dateFrom': new FormControl(null, []),
+      'dateTo': new FormControl(null, []),
+      'byActivate': new FormControl('activate', [])
+    });
   }
 
   getDetail(id: number) {
